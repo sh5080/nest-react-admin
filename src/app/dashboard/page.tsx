bf1523a60da7bf9628ee80f4971af1d407614119
sh5080 <prpn97@gmail.com>
2024-03-15 17:31:52 +0900

    feat: 메인 레이아웃 적용

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,63 +10,66 @@ import { TasksProgress } from '../../components/dashboard/overview/tasks-progres
 import { TotalCustomers } from '../../components/dashboard/overview/total-customers';
 import { TotalProfit } from '../../components/dashboard/overview/total-profit';
 import { Traffic } from '../../components/dashboard/overview/traffic';
+import Layout from './layout';
 
 export default function Page() {
   return (
-    <Grid container spacing={3}>
-      <Grid item lg={3} sm={6} xs={12}>
-        <Budget diff={12} trend="up" sx={{ height: '100%' }} value="$24k" />
+    <Layout>
+      <Grid container spacing={3}>
+        <Grid item lg={3} sm={6} xs={12}>
+          <Budget diff={12} trend="up" sx={{ height: '100%' }} value="$24k" />
+        </Grid>
+        <Grid item lg={3} sm={6} xs={12}>
+          <TotalCustomers diff={16} trend="down" sx={{ height: '100%' }} value="1.6k" />
+        </Grid>
+        <Grid item lg={3} sm={6} xs={12}>
+          <TasksProgress sx={{ height: '100%' }} value={75.5} />
+        </Grid>
+        <Grid item lg={3} sm={6} xs={12}>
+          <TotalProfit sx={{ height: '100%' }} value="$15k" />
+        </Grid>
+        <Grid item lg={8} xs={12}>
+          <Sales
+            chartSeries={[
+              { name: 'This year', data: [18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18, 20] },
+              { name: 'Last year', data: [12, 11, 4, 6, 2, 9, 9, 10, 11, 12, 13, 13] },
+            ]}
+            sx={{ height: '100%' }}
+          />
+        </Grid>
+        <Grid item lg={4} md={6} xs={12}>
+          <Traffic chartSeries={[63, 15, 22]} labels={['Desktop', 'Tablet', 'Phone']} sx={{ height: '100%' }} />
+        </Grid>
+        <Grid item lg={4} md={6} xs={12}>
+          <LatestProducts
+            products={[
+              {
+                id: 'PRD-005',
+                name: 'Soja & Co. Eucalyptus',
+                image: '/assets/product-5.png',
+                updatedAt: dayjs().subtract(18, 'minutes').subtract(5, 'hour').toDate(),
+              },
+              // Other products...
+            ]}
+            sx={{ height: '100%' }}
+          />
+        </Grid>
+        <Grid item lg={8} md={12} xs={12}>
+          <LatestOrders
+            orders={[
+              {
+                id: 'ORD-007',
+                customer: { name: 'Ekaterina Tankova' },
+                amount: 30.5,
+                status: 'pending',
+                createdAt: dayjs().subtract(10, 'minutes').toDate(),
+              },
+              // Other orders...
+            ]}
+            sx={{ height: '100%' }}
+          />
+        </Grid>
       </Grid>
-      <Grid item lg={3} sm={6} xs={12}>
-        <TotalCustomers diff={16} trend="down" sx={{ height: '100%' }} value="1.6k" />
-      </Grid>
-      <Grid item lg={3} sm={6} xs={12}>
-        <TasksProgress sx={{ height: '100%' }} value={75.5} />
-      </Grid>
-      <Grid item lg={3} sm={6} xs={12}>
-        <TotalProfit sx={{ height: '100%' }} value="$15k" />
-      </Grid>
-      <Grid item lg={8} xs={12}>
-        <Sales
-          chartSeries={[
-            { name: 'This year', data: [18, 16, 5, 8, 3, 14, 14, 16, 17, 19, 18, 20] },
-            { name: 'Last year', data: [12, 11, 4, 6, 2, 9, 9, 10, 11, 12, 13, 13] },
-          ]}
-          sx={{ height: '100%' }}
-        />
-      </Grid>
-      <Grid item lg={4} md={6} xs={12}>
-        <Traffic chartSeries={[63, 15, 22]} labels={['Desktop', 'Tablet', 'Phone']} sx={{ height: '100%' }} />
-      </Grid>
-      <Grid item lg={4} md={6} xs={12}>
-        <LatestProducts
-          products={[
-            {
-              id: 'PRD-005',
-              name: 'Soja & Co. Eucalyptus',
-              image: '/assets/product-5.png',
-              updatedAt: dayjs().subtract(18, 'minutes').subtract(5, 'hour').toDate(),
-            },
-            // Other products...
-          ]}
-          sx={{ height: '100%' }}
-        />
-      </Grid>
-      <Grid item lg={8} md={12} xs={12}>
-        <LatestOrders
-          orders={[
-            {
-              id: 'ORD-007',
-              customer: { name: 'Ekaterina Tankova' },
-              amount: 30.5,
-              status: 'pending',
-              createdAt: dayjs().subtract(10, 'minutes').toDate(),
-            },
-            // Other orders...
-          ]}
-          sx={{ height: '100%' }}
-        />
-      </Grid>
-    </Grid>
+    </Layout>
   );
 }
