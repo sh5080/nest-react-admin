816cc35858fadb6d1c4302e4f5ded689c1ce11d9
sh5080 <prpn97@gmail.com>
2024-03-16 22:30:12 +0900

    feat: recoil setting

--- src/states/user.state.ts
+++ src/states/user.state.ts
@@ -1,6 +1,9 @@
 import { atom } from 'recoil';
 
+import { persistAtom } from './index';
+
 export const userState = atom({
   key: 'userState',
   default: { nickname: '', role: '' },
+  effects_UNSTABLE: [persistAtom],
 });
